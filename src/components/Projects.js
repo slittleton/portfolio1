7f85df21446b3556a53528789548407dcceb1ad9
slittleton <littletonltp@gmail.com>
2019-08-31 19:41:29 -0700

    projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,19 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+import "../css/projects.css";
+import catcalc from '../img/catcalc1.png'
 
 const Projects = () => {
-
-  return(
+  return (
     <div className="projects">
-      Projects
+      <div className="title projects-title">Projects</div>
+      <div className="project-card">
+        <div className="card-container">
+          <div className="project-title">Cat Calculator</div>
+          <img src={catcalc} alt="" className="project-img"/>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Projects;
