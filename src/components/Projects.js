bc07f25982bee6e5cc9612ad2cb1182744a9a2ba
slittleton <littletonltp@gmail.com>
2019-09-04 12:33:53 -0700

    projectspage

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,53 @@
 import React, { useState, useEffect } from "react";
 import "../css/projects.css";
-import catcalc from '../img/catcalc1.png'
+import catcalc from "../img/catcalc1.png";
+import pickydex1 from "../img/pickydex1.png";
+import ecomm1 from "../img/ecomm1.png";
+import ProjectCard from "./layout/ProjectCard";
+
+const project1 = {
+  projectName: "Calculator Cat",
+  projectImg: catcalc,
+  type: "Mobile, Front End, UX/UI",
+  lang: "Javascript",
+  framework: "React, React-Native",
+  other: "react-native-community/async-storage 1.5.1",
+  githubLink: "https://github.com/slittleton/CatCalculator2",
+  projectLink: "https://play.google.com/store/apps/details?id=com.catcalculator"
+};
+const project2 = {
+  projectName: "Ecommerce Website",
+  projectImg: ecomm1,
+  type: "Front End, Back End",
+  lang: "Javascript",
+  framework: "React",
+  runTimeEnvironment: "Node Js",
+  other: "express, mongoDB, mongoose, jsonwebtoken, joi, bcrypt, config, redux, dotenv, braintree, query-string, react-redux, redux-thunk ",
+  githubLink: "https://github.com/slittleton/ecomm1",
+  projectLink: "https://github.com/slittleton/ecomm1"
+};
+const project3 = {
+  projectName: "PickyDex",
+  projectImg: pickydex1,
+  type: "Mobile, Front End, UX/UI",
+  lang: "Javascript",
+  framework: "React, React-Native",
+  other: "redux, react-redux, redux-thunk, react-native-community/async-storage, react-native-gesture-handler, react-navigation",
+  githubLink: "https://github.com/slittleton/pickydexNative",
+  projectLink: "https://play.google.com/store/apps/details?id=com.pickydexnative"
+};
 
 const Projects = () => {
   return (
     <div className="projects">
-      <div className="title projects-title">Projects</div>
-      <div className="project-card">
-        <div className="card-container">
-          <div className="project-title">Cat Calculator</div>
-          <img src={catcalc} alt="" className="project-img"/>
-        </div>
+      <div className="projects-title-wrapper">
+        <div className="title projects-title">Projects</div>
+      </div>
+
+      <div className="projects-container">
+        <ProjectCard project={project1} />
+        <ProjectCard project={project2} />
+        <ProjectCard project={project3} />
       </div>
     </div>
   );
---
+++ src/components/layout/ProjectCard.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+
+const ProjectCard = props => {
+  const [value, setValue] = useState(false);
+
+  const {
+    projectName,
+    projectImg,
+    type,
+    lang,
+    framework,
+    other,
+    githubLink,
+    projectLink,
+    runTimeEnvironment
+  } = props.project;
+
+  const changeTrans = () => {
+    value ? setValue(false) : setValue(true);
+  };
+
+  return (
+    <div
+      className="project-card"
+      onMouseEnter={changeTrans}
+      onMouseLeave={changeTrans}
+    >
+      <div className="card-container">
+        <div className="project-title">{projectName}</div>
+
+        <div className="img-container">
+          <img src={projectImg} alt="cat calculator" className="project-img" />
+        </div>
+        <CSSTransition in={value} timeout={1000} classNames="box">
+          <div className="box">
+            <div className="info-block">
+              <div className="info-subtitle">Project Type</div>
+              <div className="info-text">{type}</div>
+            </div>
+            <div className="info-block">
+              <div className="info-subtitle">{`Language(s)`}</div>
+              <div className="info-text">{lang}</div>
+            </div>
+            <div className="info-block">
+              <div className="info-subtitle">{`Framework(s)`}</div>
+              <div className="info-text">{framework}</div>
+            </div>
+            {runTimeEnvironment ? (
+              <div className="info-block">
+                <div className="info-subtitle">{`Runtime Environment`}</div>
+                <div className="info-text">{runTimeEnvironment}</div>
+              </div>
+            ) : null}
+            <div className="info-block">
+              <div className="info-subtitle">Other Tech</div>
+              <div className="info-text">{other}</div>
+            </div>
+            <div className="info-block">
+              <div className="info-subtitle">Github Link</div>
+              <a className="info-text a-link" href={githubLink}>
+                {projectName} Github
+              </a>
+            </div>
+            <div className="info-block">
+              <div className="info-subtitle">Project Link</div>
+              <a className="info-text a-link" href={projectLink}>
+                {projectName} Project
+              </a>
+            </div>
+          </div>
+        </CSSTransition>
+      </div>
+    </div>
+  );
+};
+export default ProjectCard;
