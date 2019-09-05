233aa17a3616438a73c4a78d6f9d31148b6e8918
slittleton <littletonltp@gmail.com>
2019-09-04 17:51:55 -0700

    styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,31 @@
-import React,{useState,useEffect} from 'react';
-import ContactForm from './forms/ContactForm';
+import React, { useState, useEffect } from "react";
+import ContactForm from "./forms/ContactForm";
 const Contact = () => {
-
-  return(
+  return (
     <div className="contact">
       <div className="title-wrapper">
         <div className="title-dark">CONTACT</div>
       </div>
-
-      <ContactForm/>
+      <div className="contact-info">
+        <div className="subtitle contact-type">Email: </div>
+        <a href="https://mail.google.com" className="a-link">
+          littletonse@gmail.com
+        </a>
+        <div className="subtitle contact-type">Github: </div>
+        <a href="https://github.com/slittleton" className="a-link">
+          slittleton
+        </a>
+        <div className="subtitle contact-type">LinkedIn: </div>
+        <a
+          href="https://www.linkedin.com/in/scott-littleton-1a1246161/"
+          className="a-link"
+        >
+          littletonse@gmail.com
+        </a>
+      </div>
+      <ContactForm />
     </div>
-  )
-}
+  );
+};
 
 export default Contact;
--- src/components/forms/ContactForm.js
+++ src/components/forms/ContactForm.js
@@ -1,15 +1,76 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
-const ContactForm = () =>{
+const ContactForm = () => {
   const [values, setValues] = useState({
-    name: '',
-    email:'',
-    message:'',
-  })
+    name: "",
+    email: "",
+    message: ""
+  });
+  const { name, email, message } = values;
 
-  return(
-    <div>Contact Form</div>
-  )
-}
+  const handleChange = name => e => {
+    setValues({ ...values, [name]: e.target.value });
+  };
+  const handleSubmit = async e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="contact-form">
+      <form onSubmit={handleSubmit} className="form">
+        <div className="form-control flx">
+          <div className="label-container">
+            <label htmlFor="name" className="form-subtitle">
+              Name
+            </label>
+          </div>
+          <input
+            type="text"
+            className="form-field"
+            onChange={handleChange("name")}
+            name="name"
+            value={name}
+          />
+        </div>
+        <div className="form-control flx">
+          <div className="label-container">
+            <label htmlFor="email" className="form-subtitle">
+              Email
+            </label>
+          </div>
+
+          <input
+            type="email"
+            className="form-field"
+            onChange={handleChange("email")}
+            name="email"
+            value={email}
+          />
+        </div>
+        <div className="form-control flx">
+          <div className="label-container">
+            <label htmlFor="message" className="form-subtitle">
+              Message
+            </label>
+          </div>
+
+          <textarea
+            rows="8"
+            cols="50"
+            type="text"
+            name="message"
+            value={message}
+            onChange={handleChange("message")}
+            className="form-msg-field "
+            placeholder="Please enter a message"
+          />
+        </div>
+        <div className="form-btn">
+          <button className="btn" type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default ContactForm;
