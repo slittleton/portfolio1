c1710b785f4a83330946c7e53fe7b6d73293ddb0
slittleton <littletonltp@gmail.com>
2019-09-20 10:00:02 -0700

    version 1.0.5

--- src/components/About.js
+++ src/components/About.js
@@ -15,11 +15,11 @@ const job1 = {
     "I was primarily engaged in designing graphics, customer support, product customization, and tech support",
   jobDetails: [
     "Graphic Design - Designed large portion of the companies graphics for use in everything from products to logos, banners, advertisements, etc",
-    "Product Creation - The nature of the company requied creating products that could be generalized with a wide variety of designs and options. More specifically, jewlery with bezel attachments that have images in them",
+    "Product Creation - The nature of the company required creating products that could be generalized with a wide variety of designs and options. More specifically, jewlery with bezel attachments that have images in them",
     "Customer Support - Handled every type of customer issue ranging from creating custom product designs to handling billing, shipping, returns and generally anything customers might need",
-    "Tech Support - Ecommerce website maintenence and improvement, handled all tech related issue, if needed to be fixed, updated, renewed, or otherwise improved",
+    "Tech Support - eCommerce website maintenance and improvement, handled all tech related issue, if needed to be fixed, updated, renewed, or otherwise improved",
     "Supply Coordination and Purchase - Applied fractional reserve principles to supply. Negotiated with overseas and local suppliers for materials including customized batches from manufacturers.",
-    "Company Management - Involved in virtually all management level decisionmaking."
+    "Company Management - Involved in virtually all management level decision making."
   ]
 };
 
